cf39f12285564b0e1756faa9b6f3673388f92509
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-11 19:13:17 +0500

    update bg of accept wristbands

--- src/pages/Home/components/BYOP.jsx
+++ src/pages/Home/components/BYOP.jsx
@@ -1,29 +1,36 @@
 const BYOP = () => {
   return (
     <section
-      className="bg-cover bg-center bg-no-repeat py-20 " // Added useful background classes
+      className="bg-white " // Added useful background classes
     >
       <div className="container  ">
-        <div className=" relative flex flex-col items-center justify-center overflow-hidden ">
-          <div className="absolute oval-shape  -z-10 right-0  "></div>
-          <div className=" flex flex-col items-center justify-center">
-            <h1 className=" font-semibold mb-4">
-              BYOP - Bring Your Own Processor
-            </h1>
-            <p className=" text-center md:w-[30rem] md:max-w-[30rem] lg:w-[56rem] lg:max-w-[56rem]  ">
-              Acceptify empowers merchants with complete processor flexibility.
-              Seamlessly integrate your preferred payment processing partner and
-              move between processors without changing a line of code.
-            </p>
-          </div>
-          <div className="2xl:w-[521.88px] 2xl:h-[363px] xl:w-[467px] xl:h-[324px] lg:w-[391px] lg:h-[269.08px] md:w-[297px] md:h-[204px]">
-            <video className="w-full h-full object-contain" autoPlay loop muted>
-              <source
-                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1733836482/components/bring_your_own_process.webm"
-                type="video/webm"
-              />
-              Your browser does not support the video tag.
-            </video>
+        <div className="py-20">
+          <div className=" relative flex flex-col items-center justify-center overflow-hidden ">
+            <div className="absolute oval-shape   right-0  "></div>
+            <div className=" flex flex-col items-center justify-center">
+              <h1 className=" font-semibold mb-4">
+                BYOP - Bring Your Own Processor
+              </h1>
+              <p className=" text-center md:w-[30rem] md:max-w-[30rem] lg:w-[56rem] lg:max-w-[56rem]  ">
+                Acceptify empowers merchants with complete processor
+                flexibility. Seamlessly integrate your preferred payment
+                processing partner and move between processors without changing
+                a line of code.
+              </p>
+            </div>
+            <div className="2xl:w-[521.88px] 2xl:h-[363px] xl:w-[467px] xl:h-[324px] lg:w-[391px] lg:h-[269.08px] md:w-[297px] md:h-[204px]">
+              <video
+                className="w-full h-full object-contain"
+                autoPlay
+                loop
+                muted>
+                <source
+                  src="https://res.cloudinary.com/dq5guzzge/video/upload/v1733836482/components/bring_your_own_process.webm"
+                  type="video/webm"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
         </div>
       </div>
