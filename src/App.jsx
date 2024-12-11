3b102bfa60c93835373702b0b020822e4526ec78
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-12 02:16:14 +0500

    add logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Fragment } from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './partials/Header'
-import Footer from './partials/Footer'
+import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./partials/Header";
+import Footer from "./partials/Footer";
 const Layout = () => {
-    // This page only contains components that should be present across all pages
-    return <Fragment>
-        <Header />
-        <main>
-            <Outlet />
-        </main>
-        <Footer />
+  // This page only contains components that should be present across all pages
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      {/* <Footer /> */}
     </Fragment>
-}
+  );
+};
 
-export default Layout
+export default Layout;
