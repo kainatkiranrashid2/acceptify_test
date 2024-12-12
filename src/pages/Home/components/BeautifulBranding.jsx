d0241523b08f670468c823df7979329261dac255
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-12 13:17:04 +0500

    update represent your brand video

--- src/pages/Home/components/BeautifulBranding.jsx
+++ src/pages/Home/components/BeautifulBranding.jsx
@@ -18,7 +18,7 @@ const BeautifulBranding = () => {
           <div className="2xl:w-[748px] 2xl:h-[453px] xl:w-[645px] xl:h-[391px] lg:w-[495.82px] lg:h-[295.26px] md:w-[371.7px] md:h-[219px] ">
             <video className="w-full h-full object-contain" autoPlay loop muted>
               <source
-                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1733836589/components/represent_your_brand_video.webm"
+                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1733991376/components/represent_your_branding.webm"
                 type="video/webm"
               />
               Your browser does not support the video tag.
