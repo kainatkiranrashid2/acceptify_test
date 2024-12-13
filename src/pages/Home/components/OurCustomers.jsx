2dfb4b072e63d5269e8794b37590c8b49b03d1de
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-13 11:26:24 +0500

    update logos

--- src/pages/Home/components/OurCustomers.jsx
+++ src/pages/Home/components/OurCustomers.jsx
@@ -19,25 +19,22 @@ const OurCustomers = () => {
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988799/components/our_customers_logo/fiserv_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988798/components/our_customers_logo/elavon_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988798/components/our_customers_logo/freedompay_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988797/components/our_customers_logo/mai_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734070353/components/our_customers_logo/globalpayment_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989402/components/our_customers_logo/ace_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988795/components/our_customers_logo/garuda_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988796/components/our_customers_logo/kaiser_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988795/components/our_customers_logo/alaska_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988799/components/our_customers_logo/fiji_logo.svg",
+
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988796/components/our_customers_logo/suncountry_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989402/components/our_customers_logo/scripps_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988794/components/our_customers_logo/jcb_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988793/components/our_customers_logo/ticketmaster_logo.svg",
+
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988793/components/our_customers_logo/nfl_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989403/components/our_customers_logo/omniair_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989404/components/our_customers_logo/globalpayments_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988793/components/our_customers_logo/airtanzania_logo.svg",
   ];
   const darkLogos = [
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988799/components/our_customers_logo/google_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734070605/components/our_customers_dark_mode/google_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065016/components/our_customers_dark_mode/apple_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065021/components/our_customers_dark_mode/samsung_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988797/components/our_customers_logo/googleplay_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734070672/components/our_customers_dark_mode/googleplay_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065022/components/our_customers_dark_mode/visa_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065019/components/our_customers_dark_mode/discover_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065018/components/our_customers_dark_mode/jcb_logo.svg",
@@ -46,23 +43,19 @@ const OurCustomers = () => {
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065016/components/our_customers_dark_mode/mastercard_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065018/components/our_customers_dark_mode/evo_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065020/components/our_customers_dark_mode/worldpay_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988800/components/our_customers_logo/chase_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734070789/components/our_customers_dark_mode/chase_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065016/components/our_customers_dark_mode/fiserv_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065019/components/our_customers_dark_mode/elavon_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065018/components/our_customers_dark_mode/freedompay_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065017/components/our_customers_dark_mode/globalpayments_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989402/components/our_customers_logo/ace_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988795/components/our_customers_logo/garuda_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988796/components/our_customers_logo/kaiser_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988795/components/our_customers_logo/alaska_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988799/components/our_customers_logo/fiji_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988796/components/our_customers_logo/suncountry_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734071104/components/our_customers_dark_mode/kaiser_logo.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734069883/components/our_customers_dark_mode/alaska_logo.svg",
+
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734069908/components/our_customers_dark_mode/suncountry_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065020/components/our_customers_dark_mode/scripps_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065019/components/our_customers_dark_mode/ticketmaster_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065020/components/our_customers_dark_mode/nfl_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989403/components/our_customers_logo/omniair_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734065023/components/our_customers_dark_mode/mai_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988793/components/our_customers_logo/airtanzania_logo.svg",
   ];
 
   return (
@@ -75,21 +68,6 @@ const OurCustomers = () => {
                 <div
                   key={index}
                   className={`xl:w-[182px] xl:!h-[100px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] md:w-[120px] md:h-[66px] bg-white dark:bg-[#1D3B9D]`}>
-                  <img
-                    src={logo}
-                    alt={`Customer logo ${index + 1}`}
-                    className="h-10 object-contain"
-                  />
-                </div>
-              ))}
-            </div>
-
-            {/* Last row centered */}
-            <div className="flex justify-center items-center mx-auto justify-items-center gap-6 mt-8">
-              {logos.slice(24).map((logo, index) => (
-                <div
-                  key={index}
-                  className={`xl:w-[182px] xl:!h-[100px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] md:w-[120px] md:h-[66px] bg-white`}>
                   <img
                     src={logo}
                     alt={`Customer logo ${index + 1}`}
