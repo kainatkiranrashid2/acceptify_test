abae014d6c1c4cd108398167d7771ec41c63e324
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-13 16:05:38 +0500

    update logos

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -32,7 +32,7 @@ const HeroSection = () => {
   );
 
   return (
-    <div className="cp-hero relative bg-gradient-to-br from-[#6E3BFB] to-[#2D79FF] dark:from-[#282478] dark:to-[#150731] overflow-x-hidden">
+    <div className="cp-hero relative bg-gradient-to-br  from-[#6E3BFB] to-[#2D79FF] dark:from-[#282478] dark:to-[#150731] overflow-x-hidden">
       {/* Main white glow effect */}
       <div className="absolute dark:hidden -top-64 -left-32 w-[376px] h-[376px] bg-white rounded-full blur-3xl"></div>
       <div className="absolute hidden dark:block -top-64 -left-32 w-[376px] h-[376px] bg-[#431C95]  rounded-full blur-3xl"></div>
@@ -40,9 +40,9 @@ const HeroSection = () => {
       {/* Secondary smaller glow for intensity */}
       <div className="absolute dark:hidden -top-5 -left-5 w-32 h-32 bg-white/30 rounded-full blur-3xl"></div>
 
-      <div className="hidden md:block relative  h-full  aspect-auto md:aspect-video container ">
+      <div className="relative hidden md:block w-full md:h-full   aspect-auto md:aspect-video container ">
         <AdvancedVideo
-          className="absolute top-0 left-0 w-full h-full object-contain "
+          className="absolute hidden md:block top-0 left-0 w-full h-full object-contain "
           autoPlay
           loop
           muted
@@ -51,7 +51,7 @@ const HeroSection = () => {
         />
 
         {/* Text overlay */}
-        <div className="ml:5 sm:ml-6 md:ml-10 lg:ml-20  relative z-10 h-full flex items-center ">
+        <div className="hidden md:flex sm:ml-6 md:ml-10 lg:ml-20  relative z-10 h-full  items-center ">
           <div className="max-w-[300px] lg:max-w-[430px]">
             <h1 className=" text-white font-semibold tracking-tighter">
               Lighting Fast Mobile Enterprise Payments Anywhere
@@ -71,6 +71,23 @@ const HeroSection = () => {
               />
             </button>
           </div>
+        </div>
+      </div>
+      <div className="mx-auto !w-[402px]  mt-[102px] text-center  md:hidden    relative  h-full flex justify-center items-center ">
+        <div className="w-full lg:max-w-[430px]">
+          <h1 className=" text-white font-semibold tracking-tighter">
+            Lighting Fast Mobile Enterprise Payments Anywhere
+          </h1>
+          <p className="text-white w-full  font-normal my-[12px] ">
+            Acceptify enables your applications to accept worldwide, secure,
+            certified, online/offline payments while removing all of your
+            security concerns.
+          </p>
+
+          <button className=" mt-4 mx-auto bg-primary hover:bg-[#51A805] text-[16px] tracking-tight leading-6  flex flex-row justify-center items-center gap-3 text-white  font-semibold px-3 py-[10px] rounded-lg  ">
+            Start Now{" "}
+            <img src="./images/arrow.svg" className="size-3 mt-[4px]" alt="" />
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Home/components/OurCustomers.jsx
+++ src/pages/Home/components/OurCustomers.jsx
@@ -62,16 +62,16 @@ const OurCustomers = () => {
     <section className="dark:bg-[#06142F]">
       <div className="container">
         <div className="py-20">
-          <div className="2xl:mx-[160px] xl:mx-8 md:mx-10 mb-[200px] relative dark:hidden">
+          <div className="2xl:mx-[160px] xl:mx-8 lg:[196px] md:mx-[104px] xl:mb-[140px] lg:mb-[150px] md:mb-[60px] relative dark:hidden">
             <div className="grid grid-cols-6 items-center justify-center gap-6">
               {logos.slice(0, 24).map((logo, index) => (
                 <div
                   key={index}
-                  className={`xl:w-[182px] xl:!h-[100px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] md:w-[120px] md:h-[66px] bg-white dark:bg-[#1D3B9D]`}>
+                  className={`2xl:w-[182px] 2xl:!h-[100px] xl:w-[150px] xl:h-[82.42px] lg:w-[95px] h-[52px] md:w-[83px] md:h-[46px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] bg-white dark:bg-[#1D3B9D]`}>
                   <img
                     src={logo}
                     alt={`Customer logo ${index + 1}`}
-                    className={`h-10 object-contain ${
+                    className={`h-8 object-contain ${
                       index === 16 ? "h-20" : ""
                     }`}
                   />
@@ -79,17 +79,17 @@ const OurCustomers = () => {
               ))}
             </div>
           </div>
-          <div className="2xl:mx-[160px] xl:mx-8 md:mx-10 mb-[200px] relative hidden dark:block">
+          <div className="2xl:mx-[160px] xl:mx-[130px] lg:mx-[196px] md:mx-12 xl:mb-[140px] lg:mb-[150px] md:mb-[60px] relative hidden dark:block">
             <div className="grid grid-cols-6 items-center justify-center gap-6">
               {darkLogos.slice(0, 24).map((logo, index) => (
                 <div
                   key={index}
-                  className={`xl:w-[182px] xl:!h-[100px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] md:w-[120px] md:h-[66px]  dark:bg-[#1D3B9D]`}>
+                  className={`2xl:w-[182px] 2xl:!h-[100px] xl:w-[150px] xl:h-[82.42px] lg:w-[95px] h-[52px] md:w-[83px] md:h-[46px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)]   dark:bg-[#1D3B9D]`}>
                   <img
                     src={logo}
                     alt={`Customer logo ${index + 1}`}
-                    className={`h-10 object-contain ${
-                      index === 16 ? "h-24" : ""
+                    className={`h-8 object-contain ${
+                      index === 16 ? "h-20" : ""
                     }`}
                   />
                 </div>
@@ -101,7 +101,7 @@ const OurCustomers = () => {
               {darkLogos.slice(24).map((logo, index) => (
                 <div
                   key={index}
-                  className={`xl:w-[182px] xl:!h-[100px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] md:w-[120px] md:h-[66px] dark:bg-[#1D3B9D]`}>
+                  className={`2xl:w-[182px] 2xl:!h-[100px] xl:w-[150px] xl:h-[82.42px] py-6 px-5 rounded-[18px]  border-1 flex justify-center items-center shadow-[0px_3px_14px_0px_rgba(74,58,255,0.03),0px_-2px_4px_0px_rgba(20,20,43,0.02),0px_12px_24px_0px_rgba(20,20,43,0.04)] md:w-[120px] md:h-[66px] dark:bg-[#1D3B9D]`}>
                   <img
                     src={logo}
                     alt={`Customer logo ${index + 1}`}
