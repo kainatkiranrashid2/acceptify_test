c73fe0b89c0050dac0099022b6234ad08d7581ed
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-17 16:06:06 +0500

    add responsiveness for sm and xs screens

--- src/pages/Home/components/PanCakeClaim.jsx
+++ src/pages/Home/components/PanCakeClaim.jsx
@@ -204,7 +204,7 @@ const VideoScrollComponent = () => {
                 </h2>
                 <p className="dark:text-white  w-full">{item.subtitle}</p>
               </div>
-              <div className="w-[320px] h-[220px] sm:w-[3px] sm:h-[182px] md:w-[182px] md:h-[120px] mx-auto mt-[60px] sm:mt-20 md:mt-[5.5rem]">
+              <div className="w-[320px] h-[220px] sm:w-[3px] sm:h-[182px] md:w-[182px] md:h-[120px] mx-auto mt-[60px] sm:mt-20 md:mt-[5.5rem] bg-transparent dark:bg-transparent">
                 <video
                   src={item.url}
                   className="w-full h-auto max-h-full object-cover "
