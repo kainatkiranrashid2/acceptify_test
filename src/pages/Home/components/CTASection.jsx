ff9e3be2d4176e1eeb26a2349053114a50d272d4
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-18 17:40:41 +0500

    update individual report

--- src/pages/Home/components/CTASection.jsx
+++ src/pages/Home/components/CTASection.jsx
@@ -13,7 +13,7 @@ const CTASection = () => {
                 mind of a fully certified payments platform. Get in touch with
                 our experts now!
               </p>
-              <button className="text-[9.6px]/[13px] sm:[11px]/[16.5px] md:text-[14px]/[20px] lg:text-[11px]/[16.5px] xl:text-[13px]/[19px] 2xl:text-[16px]/[24px] mt-5 bg-primary hover:bg-[#51A805] rounded-lg py-[10px] px-[14px] text-white ">
+              <button className="text-[9.6px]/[13px] sm:[11px]/[16.5px] md:text-[14px]/[20px] lg:text-[11px]/[16.5px] xl:text-[13px]/[19px] 2xl:text-[16px]/[24px] mt-5 bg-primary hover:bg-[#51A805] rounded-lg py-[5px] px-[9px] text-white ">
                 Contact an Expert
               </button>
             </div>
