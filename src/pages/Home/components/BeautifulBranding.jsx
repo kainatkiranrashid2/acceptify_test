b98cb43fe7b8aa5b292edd1d29a4bc2be6d0ebe1
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-19 13:29:13 +0500

    -------

--- src/pages/Home/components/BeautifulBranding.jsx
+++ src/pages/Home/components/BeautifulBranding.jsx
@@ -18,19 +18,21 @@ const BeautifulBranding = () => {
             </p>
           </div>
           <div className="2xl:w-[748px] 2xl:h-[453px] xl:w-[645px] xl:h-[391px] lg:w-[495.82px] lg:h-[295.26px] md:w-[361.7px] md:h-[219px] sm:w-[375px] sm:h-[222px] ">
-            <video className="w-full h-full object-contain" autoPlay
-            loop
-            muted
-            controlsList="nodownload" // Prevents download option in controls
-            disablePictureInPicture // Disables picture-in-picture mode
-            playsInline // Better mobile experience
-            onContextMenu={(e) => e.preventDefault()}>
+            <video
+              className="w-full h-full object-contain"
+              autoPlay
+              loop
+              muted
+              controlsList="nodownload" // Prevents download option in controls
+              disablePictureInPicture // Disables picture-in-picture mode
+              playsInline // Better mobile experience
+              onContextMenu={(e) => e.preventDefault()}>
               <source
-                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1733991376/components/represent_your_branding.webm"
+                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1734596300/components/represent_your_brand/ryb_hev.mov"
                 type="video/webm"
               />
               <source
-                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1734596300/components/represent_your_brand/ryb_hev.mov"
+                src="https://res.cloudinary.com/dq5guzzge/video/upload/v1733991376/components/represent_your_branding.webm"
                 type="video/webm"
               />
               Your browser does not support the video tag.
