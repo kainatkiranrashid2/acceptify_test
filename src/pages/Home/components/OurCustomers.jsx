598cbffcaba6ac4efd3399a0b356bc6725a8159e
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-20 17:53:54 +0500

    ------

--- src/pages/Home/components/OurCustomers.jsx
+++ src/pages/Home/components/OurCustomers.jsx
@@ -18,7 +18,7 @@ const OurCustomers = () => {
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988799/components/our_customers_logo/fiserv_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988798/components/our_customers_logo/elavon_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988798/components/our_customers_logo/freedompay_logo.svg",
-    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734696695/components/our_customers_logo/global_payments.svg",
+    "https://res.cloudinary.com/dq5guzzge/image/upload/v1734698706/components/our_customers_logo/global_payments_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733989402/components/our_customers_logo/ace_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988796/components/our_customers_logo/kaiser_logo.svg",
     "https://res.cloudinary.com/dq5guzzge/image/upload/v1733988795/components/our_customers_logo/alaska_logo.svg",
@@ -70,7 +70,11 @@ const OurCustomers = () => {
                   <img
                     src={logo}
                     alt={`Customer logo ${index + 1}`}
-                    className="h-9  object-contain "
+                    className={`${
+                      index === 16
+                        ? "h-[45px] object-cover"
+                        : "h-8  object-contain "
+                    }`}
                   />
                 </div>
               ))}
@@ -85,7 +89,11 @@ const OurCustomers = () => {
                   <img
                     src={logo}
                     alt={`Customer logo ${index + 1}`}
-                    className={`h-7 md:h-8 object-contain `}
+                    className={`${
+                      index === 16
+                        ? "h-[44px] object-cover"
+                        : "h-8  object-contain "
+                    }`}
                   />
                 </div>
               ))}
