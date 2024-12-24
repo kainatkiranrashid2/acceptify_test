e0ceeca5e78ea2baa12f5bc82a92032faef87901
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-24 13:17:41 +0500

    ------------

--- src/pages/Home/components/PanCakeClaim.jsx
+++ src/pages/Home/components/PanCakeClaim.jsx
@@ -235,10 +235,10 @@ const VideoScrollComponent = () => {
                 </h2>
                 <p className="dark:text-white w-full">{item.subtitle}</p>
               </div>
-              <div className="w-[320px] h-[220px] sm:w-[489px] sm:h-[182px] md:w-[489px] md:h-[300px] mx-auto mt-[60px] sm:mt-20 md:mt-[5.5rem] bg-transparent dark:bg-transparent">
+              <div className="w-[280px] h-[220px] sm:w-[489px] sm:h-[182px] md:w-[489px] md:h-[300px] mx-auto mt-[60px] sm:mt-20 md:mt-[5.5rem] bg-transparent dark:bg-transparent">
                 <LoadingPancakeVideo
                   src={videoUrls[index]}
-                  className="w-full h-auto max-h-full object-cover bg-transparent"
+                  className="w-full h-full max-h-full object-contain sm:object-cover bg-transparent"
                   autoPlay
                   loop
                   muted
