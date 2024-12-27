ddf55ba18ece6b4df3b9014105b2a421e64e3188
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-27 10:28:33 +0500

    -----

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -102,7 +102,7 @@ const Header = () => {
             {/* Logo section */}
             <a
               href="/"
-              className="w-[75px] h-[20px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  flex dark:hidden items-center">
+              className="w-[83px] h-[24px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  flex dark:hidden items-center">
               <img
                 src="https://res.cloudinary.com/dq5guzzge/image/upload/v1733812836/components/acceptifylogo_black.png"
                 className="bg-transparent h-full w-auto object-contain"
@@ -112,7 +112,7 @@ const Header = () => {
 
             <a
               href="/"
-              className="w-[75px] h-[20px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  dark:flex hidden items-center">
+              className="w-[83px] h-[24px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  dark:flex hidden items-center">
               <img
                 src="https://res.cloudinary.com/dq5guzzge/image/upload/v1733812935/components/acceptifylogo_white.png"
                 className="bg-transparent h-full w-auto object-contain"
@@ -168,7 +168,7 @@ const Header = () => {
                       <div>
                         <a
                           href="/"
-                          className="w-[75px] h-[20px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  block dark:hidden items-center">
+                          className="w-[83px] h-[24px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  block dark:hidden items-center">
                           <img
                             src="https://res.cloudinary.com/dq5guzzge/image/upload/v1733812836/components/acceptifylogo_black.png"
                             className="bg-transparent h-full w-auto object-contain"
@@ -177,7 +177,7 @@ const Header = () => {
                         </a>
                         <a
                           href="/"
-                          className="w-[75px] h-[20px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  dark:flex hidden items-center ">
+                          className="w-[83px] h-[24px] sm:w-[93px] sm:h-[26px] xl:w-[132px] xl:h-[36px]  dark:flex hidden items-center ">
                           <img
                             src="https://res.cloudinary.com/dq5guzzge/image/upload/v1733812935/components/acceptifylogo_white.png"
                             className="bg-transparent h-full w-auto object-contain"
