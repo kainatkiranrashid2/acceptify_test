9fa0db0457257558a5aaa5c8bd69dc13cab5f0d5
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2024-12-31 13:49:12 +0500

    update branding component

--- src/pages/Home/components/BeautifulBranding.jsx
+++ src/pages/Home/components/BeautifulBranding.jsx
@@ -1,34 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import { supportsHEVCAlpha } from "../../../CheckBrowserCapability/index.js";
 import LoadingVideo from "../../../partials/LoadingVideo.jsx";
+import CloudinaryResponsiveVideo from "../../../partials/CloudinaryResponsiveVideo.jsx";
 
 const BeautifulBranding = () => {
   const videoRef = useRef(null);
   const [videoError, setVideoError] = useState(false);
   const [videoSrc, setVideoSrc] = useState("");
-
-  useEffect(() => {
-    const src = supportsHEVCAlpha()
-      ? "https://res.cloudinary.com/dq5guzzge/video/upload/v1734596300/components/represent_your_brand/ryb_hev.mov"
-      : "https://res.cloudinary.com/dq5guzzge/video/upload/v1733991376/components/represent_your_branding.webm";
-
-    setVideoSrc(src);
-
-    return () => {
-      if (videoRef.current) {
-        videoRef.current.pause();
-        videoRef.current.src = "";
-      }
-    };
-  }, []);
-
-  const handleLoadedData = () => {
-    console.log("Video loaded successfully");
-  };
+  const hevcVideo =
+    "https://res.cloudinary.com/dq5guzzge/video/upload/v1734596300/components/represent_your_brand/ryb_hev.mov";
+  const webMVideo =
+    "https://res.cloudinary.com/dq5guzzge/video/upload/v1733991376/components/represent_your_branding.webm";
+  const hevcMobile =
+    "https://res.cloudinary.com/dq5guzzge/video/upload/v1735631070/components/represent_your_brand/mobile_version/represent_your_brand.mov";
 
   const handleError = () => {
     console.error("Video failed to load");
     setVideoError(true);
+  };
+  const handleVideoLoaded = () => {
+    console.log(`Video  loaded successfully`);
   };
 
   return (
@@ -48,23 +39,21 @@ const BeautifulBranding = () => {
               colors.
             </p>
           </div>
-          <div className="2xl:w-[748px] 2xl:h-[453px] xl:w-[645px] xl:h-[391px] lg:w-[495.82px] lg:h-[295.26px] md:w-[361.7px] md:h-[219px] sm:w-[375px] sm:h-[222px]">
-            {videoSrc && !videoError && (
-              <LoadingVideo
-                className="w-full h-full object-contain"
-                src={videoSrc}
-                ref={videoRef}
-                autoPlay
-                loop
-                muted
-                controlsList="nodownload"
-                disablePictureInPicture
-                playsInline
-                onLoadedData={handleLoadedData}
-                onError={handleError}
-                onContextMenu={(e) => e.preventDefault()}
-              />
-            )}
+          <div className="mt-10 2xl:w-[748px] 2xl:h-[453px] xl:w-[645px] xl:h-[391px] lg:w-[495.82px] lg:h-[295.26px] md:w-[361.7px] md:h-[219px] sm:w-[375px] sm:h-[222px]">
+            <CloudinaryResponsiveVideo
+              ref={videoRef}
+              className="h-full w-full object-contain"
+              autoPlay
+              loop
+              muted
+              playsInline
+              hevcVideo={hevcVideo}
+              hevcMobile={hevcMobile}
+              webMVideo={webMVideo}
+              onError={handleError}
+              onContextMenu={(e) => e.preventDefault()}
+              onLoadedData={() => handleVideoLoaded()}
+            />
           </div>
         </div>
       </div>
