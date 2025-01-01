44297469fae408d8cf322506a648ba6de7297b58
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-01 16:41:51 +0500

    update wristband component

--- src/partials/CloudinaryResponsiveVideo.jsx
+++ src/partials/CloudinaryResponsiveVideo.jsx
@@ -59,7 +59,7 @@ const CloudinaryResponsiveVideo = forwardRef(
     useEffect(() => {
       const options = {
         root: null,
-        rootMargin: "200px", // Start loading when video is 50px from viewport
+        rootMargin: "100%", // Start loading when video is 50px from viewport
         threshold: 0.1,
       };
 
