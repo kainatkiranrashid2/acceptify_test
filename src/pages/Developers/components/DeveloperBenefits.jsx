943bcf15fcf40b47a80876dfc9dcb86ca9119a37
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-03 16:28:20 +0500

    ------------

--- src/pages/Developers/components/DeveloperBenefits.jsx
+++ src/pages/Developers/components/DeveloperBenefits.jsx
@@ -53,7 +53,7 @@ const DeveloperBenefits = () => {
       <div className="container ">
         <div className="pt-[100px] pb-[120px]  ">
           <div className="absolute right-0 bottom-0 opacity-10 dark:opacity-50 developerBenefit__oval"></div>
-          <div className="absolute right-0 bottom-[25%] opacity-10 dark:opacity-50 developerBenefit__oval"></div>
+          <div className="absolute right-0 bottom-[25%] opacity-10 dark:opacity-30 developerBenefit__oval"></div>
           <div className="absolute z-100 left-0 bottom-[15%] developerBenefit__ovalLeft"></div>
           <h2 className="text-center mb-10 dark:text-white">
             Developer Benefits
