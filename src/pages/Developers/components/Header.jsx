ead83b124a2eb2f0537300fdb9f2cf7f9ff9c827
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-06 16:07:33 +0500

    ----------

--- src/pages/Developers/components/Header.jsx
+++ src/pages/Developers/components/Header.jsx
@@ -21,7 +21,7 @@ paymentEngine.StartTransaction(transaction, (transactionResult, transactionRespo
     <section className="flex flex-col justify-center items-center 2xl:h-[1000px] xl:h-[850px] lg:h-[800px] w-full  bg-cover bg-center relative bg-no-repeat py-16 md:py-20 bg-[url(https://res.cloudinary.com/dq5guzzge/image/upload/v1735800494/components/developers_header/bg_light.png)]">
       <div className="xl:w-[893px] lg:w-[772px] md:w-[632px]  sm:w-[486px] flex flex-col justify-center items-center gap-5 mb-8">
         <h1 className="text-white text-center xl:w-[893px] lg:w-[772px] md:w-[636px]  sm:w-[432px]  capitalize">
-          Worldwide secure processing with a few lines of code.
+          Worldwide secure processing with a few lines of code
         </h1>
         <p className="text-white  2xl:w-[879px] xl:w-[658px] lg:w-[580px] md:w-[630px] sm:w-[433px] text-center">
           Empower your creativity with Acceptify, a cutting-edge coding tool
