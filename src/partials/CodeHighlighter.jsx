f33184d3816019c48503d1a1ad7112062d596272
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-06 17:30:50 +0500

    ----------

--- src/partials/CodeHighlighter.jsx
+++ src/partials/CodeHighlighter.jsx
@@ -116,17 +116,19 @@ paymentEngine.StartTransaction(transaction, (transactionResult, transactionRespo
       </style>
 
       <div className="2xl:w-[968px] xl:w-[793px] lg:w-[688px] md:w-[580px] sm:w-full sm:h-[240px] md:h-[283px] xl:h-[356px] lg:[336px] h-[215px]  w-[288px] rounded-lg overflow-hidden bg-[#22212c]">
-        <div className="flex items-center justify-between px-4 py-2 bg-[#22212c]  ">
+        <div className="flex items-center justify-between px-2 md:px-4 py-1 md:py-2 bg-[#22212c]  ">
           <div className="flex space-x-2">
-            <div className="w-1 h-1 sm:w-2 sm:h-2 md:w-3 md:h-3 rounded-full bg-red-500" />
-            <div className="w-1 h-1 sm:w-2 sm:h-2 md:w-3 md:h-3 rounded-full bg-yellow-500" />
-            <div className="w-1 h-1 sm:w-2 sm:h-2 md:w-3 md:h-3 rounded-full bg-green-500" />
+            <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500" />
+            <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-yellow-500" />
+            <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-green-500" />
           </div>
           <button
             onClick={copyToClipboard}
             className="text-zinc-400 hover:text-white transition-colors"
             aria-label="Copy code">
-            <FaRegCopy size={16} />
+            <div className="w-3 h-3 sm:w-4 sm:h-4 md:w-5 md:h-5 lg:w-6 lg:h-6">
+              <FaRegCopy className="w-full h-full" />
+            </div>
           </button>
         </div>
         <div className="p-1 sm:p-2 md:p-4 h-[calc(215px-10px)] sm:h-[calc(237px-18px)] lg:h-[calc(336px-36px)] xl:h-[calc(356px-36px)] md:h-[calc(283px-36px)]">
