bcc181542aa4fb0be48641fd512e3ea04898eb11
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-07 14:37:05 +0500

    ----------

--- src/pages/Developers/components/DeveloperBenefits.jsx
+++ src/pages/Developers/components/DeveloperBenefits.jsx
@@ -11,19 +11,19 @@ const DeveloperBenefits = () => {
     {
       heading: "Simplified Development",
       description:
-        "The Acceptify SDK eliminates the need for developers to write level code to handle hardware and payment processor integrations. With an intuitive API, integrating a PCI-PTS certified payment device into a P2PE validate solution is seamless.",
+        "The Acceptify SDK eliminates the need for developers to write complex code to handle hardware and payment processor integrations. With an intuitive API, integrating a PCI-PTS certified payment devices into a P2PE validated solution is seamless.",
       src: "https://res.cloudinary.com/dq5guzzge/image/upload/v1735888329/components/developers_benefits/simplified_development.svg",
     },
     {
       heading: "Enhanced Security",
       description:
-        "Acceptify is PCI-DSS and P2PE certified, ensuring that sensitive payment data is encrypted from the point of interactions, proving you with the highest level of security and taking your code our of scope for PCI Audits.",
+        "Acceptify is PCI-DSS and P2PE certified, ensuring that sensitive payment data is encrypted from the point of interaction, providing you with the highest level of security and taking your code out of scope for PCI Audits.",
       src: "https://res.cloudinary.com/dq5guzzge/image/upload/v1735888204/components/developers_benefits/enhanced_security.svg",
     },
     {
       heading: "Payment Processor Portability",
       description:
-        "Acceptify integrates with all major payment processors, ideal for software vendors as it allows customers to bring their own processor. Simplified development and a single code base to maintain.",
+        "Acceptify integrates with all major payment processors. Ideal for software vendors allowing simplified development. Ideal for customers allowing them to bring their processors. ",
       src: "https://res.cloudinary.com/dq5guzzge/image/upload/v1735888318/components/developers_benefits/payment_processor.svg",
     },
     {
@@ -35,7 +35,7 @@ const DeveloperBenefits = () => {
     {
       heading: "Future Proof Design ",
       description:
-        "Acceptify ensures that your payment integration remains robust and compliant with evolving standards. The future proofing reduces the amount of time needed maintain code, protecting your investment. ",
+        "Acceptify ensures that your payment integration remains robust and compliant with evolving standards. The future-proofing reduces the amount of time needed to maintain code, protecting your investment. ",
       src: "https://res.cloudinary.com/dq5guzzge/image/upload/v1735888243/components/developers_benefits/future_design_proof.svg",
     },
     {
