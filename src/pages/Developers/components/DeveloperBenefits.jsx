3d296741254a23c8561dce64e324994176222ce2
Kainat Kiran Rashid <kainatkiranrashid2@gmail.com>
2025-01-09 20:35:02 +0500

    ----------

--- src/pages/Developers/components/DeveloperBenefits.jsx
+++ src/pages/Developers/components/DeveloperBenefits.jsx
@@ -83,7 +83,7 @@ const DeveloperBenefits = () => {
           <h2 className="text-center mb-10  dark:text-white">
             Developer Benefits
           </h2>
-          <div className="relative px-10 py-9 md:py-9 md:px-10 gap-8 md:gap-6 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 overflow-hidden dark:bg-[#05122C] border-[1px] border border-[#3479FD] rounded-[20px]">
+          <div className="relative px-10 py-9 md:py-9 md:px-10 gap-8 md:gap-6 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 overflow-hidden dark:bg-[#05122C] border-2 border-[#3479FD] rounded-[20px]">
             <div className="absolute -top-48 dark:-top-56 -left-28 overflow-hidden developerBenefit__topGradient"></div>
             <div className="absolute -bottom-52 -right-20 opacity-20   developerBenefit__topGradient"></div>
 
@@ -124,7 +124,7 @@ const DeveloperBenefits = () => {
                 Visit GitHub Repository
               </button>
               <button
-                className="bg-transparent border-1 border-[#5D55F9] w-fit text-[#5D55F9] px-[14px]  py-[10px] text-[16px]/[27px]  rounded-sm md:rounded-lg"
+                className="bg-transparent border-[2px] border-[#5D55F9] w-fit text-[#5D55F9] px-[14px]  py-[10px] text-[16px]/[27px]  rounded-sm md:rounded-lg"
                 style={{ position: "relative", zIndex: 20 }}
                 onClick={handleClick}>
                 Visit GitHub Repository
